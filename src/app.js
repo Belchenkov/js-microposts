9a7ca2b22fe393bcd4b6b828237d468dcdf6d64c
Aleksey Belchenkov <u608110@gmail.com>
2020-03-26 15:39:05 +0300

    Edit Post - Fill Inputs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get Posts
 function getPosts() {
     http.get(http.url)
@@ -33,4 +36,24 @@ function submitPost() {
             getPosts();
         })
         .catch(err => console.log(err));
+}
+
+// Enable Edit State
+function enableEdit(e) {
+    e.preventDefault();
+
+    if (e.target.parentElement.classList.contains('edit')) {
+        const id = e.target.parentElement.dataset.id;
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+
+        const data = {
+            id,
+            title,
+            body
+        };
+
+        // Fill form with current post
+        ui.fillForm(data);
+    }
 }
